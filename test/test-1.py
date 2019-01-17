2a3a3efde4e70228d77cdeafc6e88ca5dffbce3c
betazz <zingara76@gmail.com>
2019-01-16 20:13:06 -0500

    first test

---
+++ test/test-1.py
@@ -0,0 +1,12 @@
+import unittest
+
+ 
+class TestOne(unittest.TestCase):
+  
+    def test_1_2_3(self):
+        self.assertEqual( 12, 12)
+ 
+    
+ 
+if __name__ == '__main__':
+    unittest.main()
